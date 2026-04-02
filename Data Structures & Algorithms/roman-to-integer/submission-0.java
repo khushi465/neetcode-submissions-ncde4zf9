6c368451cbce9d5b4013349c559902c5c86c6318
khushi465 <138616763+khushi465@users.noreply.github.com>
2026-04-02 09:41:11 +0530

    Add: roman-to-integer - submission-0

---
+++ Data Structures & Algorithms/roman-to-integer/submission-0.java
@@ -0,0 +1,50 @@
+class Solution {
+    public int romanToInt(String s) {
+        int sum=0;
+        HashMap<Character, Integer> map=new HashMap<>();
+        map.put('I',1);
+        map.put('I',1);
+        map.put('I',1);
+        map.put('I',1);
+        map.put('I',1);
+        map.put('I',1);
+        map.put('I',1);
+        for(int i=0;i<s.length();i++){
+            char c=s.charAt(i);
+            if(c=='I'){
+                if(i+1<s.length()&&(s.charAt(i+1)=='X'||s.charAt(i+1)=='V')){
+                    sum+=-1;
+                }else{
+                    sum+=1;
+                }
+            }
+           else if(c=='V'){
+                sum+=5;
+            }
+            else if(c=='X'){
+                if(i+1<s.length()&&(s.charAt(i+1)=='C'||s.charAt(i+1)=='L')){
+                    sum+=-10;
+                }else{
+                    sum+=10;
+                }
+            }
+            else if(c=='L'){
+                sum+=50;
+            }
+            else if(c=='C'){
+                if(i+1<s.length()&&(s.charAt(i+1)=='D'||s.charAt(i+1)=='M')){
+                    sum+=-100;
+                }else{
+                    sum+=100;
+                }
+            }
+            else if(c=='D'){
+                sum+=500;
+            }
+            else if(c=='M'){
+                sum+=1000;
+            }
+        }
+        return sum;
+    }
+}
